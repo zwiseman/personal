3a4b656adb59ada6efc609a386fae327f69aef20
Zachary Wiseman <zack21wiseman@gmail.com>
2018-04-09 21:10:56 -0400

    adding collapseable button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { WeatherComponent } from './weather/weather.component';
 import { WeatherService } from './services/weather.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 
 const routes: Routes = [
   {path: '', redirectTo: '/info', pathMatch: 'full'},
   {path: 'info', component: InfoComponent},
   {path: 'weather', component: WeatherComponent}
-]
+];
 
 @NgModule({
   declarations: [
@@ -27,7 +28,9 @@ const routes: Routes = [
     BrowserModule,
     AngularFontAwesomeModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes, {useHash: true}),
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [ WeatherService ],
   bootstrap: [AppComponent]
