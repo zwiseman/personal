73dbaabe2ef7f34e3fc5e73c73e443118c40dea7
Zachary Wiseman <zack21wiseman@gmail.com>
2018-12-09 23:24:03 -0500

    cleaning up weather and adding the algorithms portion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { InfoComponent, WeatherComponent } from 'components/index';
+import { InfoComponent, WeatherComponent, AlgorithmsComponent } from 'components/index';
 
 const routes: Routes = [
   {path: '', redirectTo: '/info', pathMatch: 'full'},
   {path: 'info', component: InfoComponent},
-  {path: 'weather', component: WeatherComponent}
+  {path: 'weather', component: WeatherComponent},
+  {path: 'algorithms', component: AlgorithmsComponent}
 ];
 
 @NgModule({
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,13 +1,19 @@
 import { WeatherComponent } from './weather/weather.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InfoComponent } from './info/info.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlgorithmsComponent } from './algorithms/algorithms.component';
 
 export * from './weather/weather.component';
 export * from './navbar/navbar.component';
 export * from './info/info.component';
+export * from './footer/footer.component';
+export * from './algorithms/algorithms.component';
 
 export const Components = [
     WeatherComponent,
     NavbarComponent,
-    InfoComponent
+    InfoComponent,
+    FooterComponent,
+    AlgorithmsComponent
 ];
